241181eabaf6762ab437c39a0882ca3d8daa4b7f
robertsmith2018 <oakcodes@gmail.com>
2025-02-25 20:31:00 +0200

    New fix to the verify.js api/src/accounts/verify.js

--- api/src/accounts/verify.js
+++ api/src/accounts/verify.js
@@ -18,7 +18,7 @@ export async function createVerifyEmailLink(email) {
     // Encode url string
     const URIencodedEmail = encodeURIComponent(email)
     // Return link for verification
-    return `https://${ROOT_DOMAIN}/verify/${URIencodedEmail}/${emailToken}`
+    return `https://${ROOT_DOMAIN}:8443/verify/${URIencodedEmail}/${emailToken}`
   } catch (e) {
     console.log("e", e)
   }
